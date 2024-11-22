12e3a7cacbd03f9df12ea1be509416c923f52069
JuanBedoya27 <juanbedoya2703@gmail.com>
2024-11-22 09:23:08 -0500

    ??

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,5 @@
 // Asegúrate de que el script se ejecute después de que la página haya cargado
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // Obtén la URL actual
     const currentLocation = window.location.pathname;
 
@@ -19,32 +19,32 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-    fetch('/HTML/nav.html')
-        .then(response => response.text())
-        .then(data => {
-            // Inserta el contenido de nav.html
-            document.getElementById('navegacion').innerHTML = data;
+fetch('/HTML/nav.html')
+    .then(response => response.text())
+    .then(data => {
+        // Inserta el contenido de nav.html
+        document.getElementById('navegacion').innerHTML = data;
 
-            // Ejecuta el script para resaltar el enlace activo
-            setActiveLink();  // Llamamos a la función para aplicar el enlace activo
-        })
-        .catch(error => console.error('Error al cargar el archivo de navegación:', error));
+        // Ejecuta el script para resaltar el enlace activo
+        setActiveLink();  // Llamamos a la función para aplicar el enlace activo
+    })
+    .catch(error => console.error('Error al cargar el archivo de navegación:', error));
 
-    // Función para agregar la clase "active" al enlace correspondiente
-    function setActiveLink() {
-        // Obtén la URL actual
-        const currentLocation = window.location.pathname;
+// Función para agregar la clase "active" al enlace correspondiente
+function setActiveLink() {
+    // Obtén la URL actual
+    const currentLocation = window.location.pathname;
 
-        // Selecciona todos los enlaces de navegación
-        const navLinks = document.querySelectorAll('nav a');
+    // Selecciona todos los enlaces de navegación
+    const navLinks = document.querySelectorAll('nav a');
 
-        // Recorrer todos los enlaces y agregar o quitar la clase 'active'
-        navLinks.forEach(link => {
-            if (currentLocation.includes(link.getAttribute('href'))) {
-                link.classList.add('active');
-            } else {
-                link.classList.remove('active');
-            }
-        });
-    }
+    // Recorrer todos los enlaces y agregar o quitar la clase 'active'
+    navLinks.forEach(link => {
+        if (currentLocation.includes(link.getAttribute('href'))) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    });
+}
 
